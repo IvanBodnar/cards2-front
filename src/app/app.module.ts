bc5755c85a604f729cdaf3a1f1e48fa95897baa0
ivanbodnar <ibodnar@outlook.com>
2018-11-13 11:49:34 -0300

    Agregado método DataService.getThemes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ThemeListComponent } from './components/themes/theme-list/theme-list.component';
 
@@ -12,7 +13,8 @@ import { ThemeListComponent } from './components/themes/theme-list/theme-list.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
 import {environment} from '../../environments/environment';
+import ThemeModel from '../models/theme.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +11,11 @@ import {environment} from '../../environments/environment';
 export class DataService {
   baseUrl = environment.baseUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getThemes(): Observable<ThemeModel> {
+    return this.http.get<ThemeModel>(this.baseUrl + 'themes');
+  }
 }
