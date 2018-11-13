654d41dd62f7147684e2f74a5bb534192050568d
ivanbodnar <ibodnar@outlook.com>
2018-11-13 14:22:08 -0300

    ThemeService obtiene la lista de themes de DataService -> fetchThemes

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+
+import {DataService} from './data.service';
+import ThemeModel from '../models/theme.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  themes: ThemeModel[];
 
-  constructor() { }
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  fetchThemes() {
+    this.dataService.getThemes()
+      .subscribe(
+        (themes: ThemeModel[]) => {
+          this.themes = themes;
+        },
+        error => console.log(error)
+      );
+  }
 }
