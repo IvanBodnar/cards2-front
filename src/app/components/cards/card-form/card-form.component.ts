f0afa270c1bad660ffd8c53d8ef16d9841d5bdda
ivan <ibodnar@outlook.com>
2018-11-15 23:50:09 -0300

    Agregados form controls en html y ts

--- src/app/components/cards/card-form/card-form.component.ts
+++ src/app/components/cards/card-form/card-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-form.component.css']
 })
 export class CardFormComponent implements OnInit {
+  cardForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.cardForm = new FormGroup({
+      'front': new FormControl(null),
+      'back': new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.cardForm);
   }
 
 }
