88a274ddacd61e5cbdf1531e7688e5dcc33c15d5
ivan <ibodnar@outlook.com>
2018-11-15 23:49:00 -0300

    Importado ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ThemeListComponent } from './components/themes/theme-list/theme-list.component';
@@ -20,7 +20,8 @@ import { CardFormComponent } from './components/cards/card-form/card-form.compon
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
