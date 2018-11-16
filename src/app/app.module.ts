18828ca51f24444fc07a12014ad69dacc708e343
ivan <ibodnar@outlook.com>
2018-11-15 23:25:34 -0300

    Agregado component card-form - Html -> form con textarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ThemeListComponent } from './components/themes/theme-list/theme-list.component';
 import { CardListComponent } from './components/cards/card-list/card-list.component';
+import { CardFormComponent } from './components/cards/card-form/card-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ThemeListComponent,
-    CardListComponent
+    CardListComponent,
+    CardFormComponent
   ],
   imports: [
     BrowserModule,
