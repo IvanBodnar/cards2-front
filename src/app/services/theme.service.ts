b0b8f012f07d2627076f2be166944673a6862242
ivan <ibodnar@outlook.com>
2018-11-17 14:30:17 -0300

    Se remueve import sin uso

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Subject} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 
 import {DataService} from './data.service';
 import ThemeModel from '../models/theme.model';
