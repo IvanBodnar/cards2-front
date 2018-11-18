b010e5296d74a54f926b6d17c2819a287d9c7d34
ivan <ibodnar@outlook.com>
2018-11-18 15:29:14 -0300

    Agregada route para shuffle component - Agregado botón de prueba en card-list html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {ThemeListComponent} from './components/themes/theme-list/theme-list.component';
 import {CardListComponent} from './components/cards/card-list/card-list.component';
 import {CardService} from './services/card.service';
+import {ShuffleComponent} from './components/cards/shuffle/shuffle.component';
 
 const routes: Routes = [
   {path: '', component: ThemeListComponent},
-  {path: 'cards/:themeName', component: CardListComponent, resolve: { cards: CardService }}
+  {path: 'cards/:themeName', component: CardListComponent, resolve: { cards: CardService }},
+  {path: 'cards/:themeName/shuffle', component: ShuffleComponent, resolve: { cards: CardService }}
 ];
 
 @NgModule({
