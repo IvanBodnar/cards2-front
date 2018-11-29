19f1e6f05c5ad69b9d7ce5d91757f58ed07b5f72
ivan <ibodnar@outlook.com>
2018-11-28 21:21:10 -0300

    Implementación de muestra de mensaje en MessageComponent

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MessageService} from '../../services/message.service';
+import MessageModel from '../../models/message.model';
+
+
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+  message: MessageModel;
+  opacity = { 'opacity': 0 };
 
-  constructor() { }
+  constructor(
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
+    this.messageService.message$
+      .subscribe(
+        ( message: MessageModel ) => {
+          this.message = message;
+          setTimeout(() => {
+            this.opacity = { 'opacity': 0.9 };
+            setTimeout(() => this.opacity = { 'opacity': 0 } , 3000);
+          }, 100);
+        }
+      );
   }
 
 }
