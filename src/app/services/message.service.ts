531bbeed20e698df7876eb27ffe6eb550a3caf57
ivan <ibodnar@outlook.com>
2018-11-28 21:19:02 -0300

    Agregado method sendMessage e implementado subject messageSubject en MessageService

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+import MessageModel from '../models/message.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  messageSubject: Subject<MessageModel> = new Subject<MessageModel>();
+  message$ = this.messageSubject.asObservable();
 
   constructor() { }
+
+  sendMessage(message: MessageModel): void {
+    this.messageSubject.next(message);
+  }
 }
